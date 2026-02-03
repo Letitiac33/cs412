67a4097202a6685a2a5142e67f209be868b7592a
Letitia Caspersen <letitiac@bu.edu>
2026-02-03 10:12:21 -0500

    added url paths for main, order, and confirmation pages for restaurant app

---
+++ restaurant/urls.py
@@ -0,0 +1,15 @@
+# File: urls.py
+# Author: Letitia Caspersen (letitiac@bu.edu), 1/28/2026
+# Description: URL patterns for Sealy's restaurant app
+
+from django.urls import path                                                                                                          
+from . import views                                                                                                                   
+                                                                                                                                        
+urlpatterns = [       
+    path('', views.restaurant, name='main'), 
+    path('main/', views.restaurant, name='main'), 
+    path('order/', views.restaurant, name='order'),
+    path('confirmation/', views.restaurant, name='confirmation'),
+                                                                                                                                                                                                
+]                                                                                                                                     
+     
