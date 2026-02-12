b68fdda64297ca58439daa57df64380cbad1653e
Letitia Caspersen <letitiac@bu.edu>
2026-02-12 11:00:37 -0500

    Created Profile model for mini_insta

---
+++ mini_insta/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    display_name = models.CharField(max_length=32)
+    profile_image_url = models.URLField(max_length=256) 
+    bio_text = models.TextField(max_length=256)
+    join_date = models.DateField(auto_now=False,auto_now_add=True)
+
+    def __str__(self):
+        return f"Profile: username='{self.username}', display name='{self.display_name}',"
