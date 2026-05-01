75ee77abcb1b70154b6c5046aedef27b42a044c0
Letitia Caspersen <letitiac@bu.edu>
2026-04-30 20:14:36 -0400

    added api key unsafe :(

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs412.settings')
+os.environ.setdefault('REMOVE_BG_API_KEY', 'XtMiBD8QpEyqgnLbPMpkDGGK')
 
 application = get_wsgi_application()
